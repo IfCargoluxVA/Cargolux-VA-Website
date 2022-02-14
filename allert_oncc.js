245576a584c5787262c5e88340670c06f3af1c7e
IfCargoluxVA <97304994+IfCargoluxVA@users.noreply.github.com>
2022-02-14 16:31:42 +0000

    Create allert_oncc.js

---
+++ allert_oncc.js
@@ -0,0 +1,6 @@
+
+
+function Join_us() {
+  alert('Only join us if you are intrested and spam applications will lead to a ban and you will be blocked')
+  window.location.href="https://airtable.com/shriYoMhNX4ThYPw4"
+}
