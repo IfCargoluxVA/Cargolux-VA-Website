dd88ed3dca0b27513e07da2020f3168287b6439a
IfCargoluxVA <97304994+IfCargoluxVA@users.noreply.github.com>
2022-02-15 10:40:32 +0000

    Update alert_oncc.js

--- alert_oncc.js
+++ alert_oncc.js
@@ -4,3 +4,7 @@ function Join_us() {
   alert('Only join us if you are intrested and spam applications will lead to a ban and you will be blocked')
   window.location.href="https://airtable.com/shriYoMhNX4ThYPw4"
 }
+
+function dicord(){
+  alert("Discord message to staff to be added but email will still be added so expect slight delays for a response.")
+}
